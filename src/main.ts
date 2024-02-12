2b4603037acaaa8358c81d4d665dff982038d18b
wpilares <wgpilaresc@gmail.com>
2024-02-11 20:33:06 -0500

    docs: added swagger & update readme

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Web Dev Trends')
+    .setDescription(
+      'Web Dev Trends API provides endpoints to fetch and manage technology news articles. Stay up-to-date with the latest trends in web development!',
+    )
+    .setVersion('1.0')
+    .addTag('trends')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
